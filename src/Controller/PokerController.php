c03f802a5123637b31c2b463515449a3c8b84bbb
benoit17630 <benoit.menier@orange.fr>
2020-11-16 17:05:36 +0100

    ajout nom prenom a la page age

--- src/Controller/PokerController.php
+++ src/Controller/PokerController.php
@@ -12,17 +12,21 @@ use Symfony\Component\Routing\Annotation\Route;
 class PokerController extends AbstractController
 {
 
-
+//creation de la page age
     /**
      * @Route("/age" ,name="age_page")
      * @param Request $request
      */
     public function age(Request $request){ // je fait une request pour choper les element de l url
-        $age = $request->query->get('age');// je dit que je recupere la variable age
+        $age = $request->query->get('age'); // je dit que je recupere la variable age
+        $nom = $request->query->get('nom');//je recupere le nom dans l url
+        $prenom = $request->query->get('prenom');//je recupere le prenom dans l url
+
         if ($age >=18){//si $age plus grand que 18 j affiche
-           die(  "bienvenue tu a plus de 18 ans");
+            echo "bienvenue : ".$nom." ".$prenom. " tu a plus de 18 ans";
+            die;
         }else{//si moin grand que 18 j affiche
-          die('degage morveux');
+            die('degage morveux');
         }
     }
     /**
@@ -36,8 +40,10 @@ class PokerController extends AbstractController
     /**
      * @route ("/contact",name="contact_page")
      */
-public  function  contact(){
+    public  function  contact(){
 
-    die("coucou");
-}
+        die("coucou");
+    }
+
+
 }
