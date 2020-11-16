fecee23cd1681d742911e3068064011fe841dc68
benoit17630 <benoit.menier@orange.fr>
2020-11-16 14:16:01 +0100

    ajout de die

--- src/Controller/PokerController.php
+++ src/Controller/PokerController.php
@@ -16,9 +16,9 @@ class PokerController
     public function age(Request $request){
         $age = $request->query->get('age');
         if ($age >=18){
-            echo "bienvenue tu a plus de 18 ans";
+           die(  "bienvenue tu a plus de 18 ans");
         }else{
-            echo "degage morveux";
+          die('degage morveux');
         }
     }
 }
